f4fc4410469c25a65974801211a06d14202aecdc
noax <thomasbacks@gmail.com>
2019-01-21 19:55:44 -0500

    R1 completed, R2 only missing A.

---
+++ Assets/Scripts/AIRespawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIRespawn : MonoBehaviour
+{
+    public bool x_axis;
+    public float offset;
+    void OnTriggerEnter(Collider other) {
+        if(x_axis) {
+            other.transform.position = new Vector3(-other.transform.position.x + offset, other.transform.position.y, other.transform.position.z);
+        } else
+        {
+            other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y, -other.transform.position.z + offset);
+            
+        }
+    }
+}
--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class NPCMovement : MonoBehaviour
 {
     public Transform Target;   // Reference the target position to chase.
+    public int AIType;
     public float speed;
     float minimum_distance = 5.0f;
+    float maximum_distance = 30.0f;
     
     void FixedUpdate() {
-        // This is the the movement configuration for A.
-        if(Vector3.Distance(transform.position, Target.position) > minimum_distance) {
-            transform.LookAt(Target);
+        // This is the movement configuration for A.
+        if(AIType == 0) {
+            MovingTowardTarget();
         }
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
 
-        
+        // This is the movement configuration for C.
+        if(AIType == 2) {
+            RunningAwayFromTarget();
+        }
+   }
+
+   void MovingTowardTarget() {
+       float step = speed * Time.deltaTime;
+       if(Vector3.Distance(transform.position, Target.position) > minimum_distance) {
+           transform.LookAt(Target);
+        //    transform.Translate(Vector3.forward * Time.deltaTime * speed);
+       }
+       transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
+   }
+   void RunningAwayFromTarget() {
+        int direction = -1;
+        if(Vector3.Distance(transform.position, Target.position) >= minimum_distance) {
+            transform.LookAt(Target);
+            transform.Rotate(0,180,0);
+        } 
+        float step = speed * Time.deltaTime * direction;
+        transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
+//    float step = speed * Time.deltaTime;
+    //    transform.position = direction * Vector3.MoveTowards(transform.position, Target.position, step);
    }
 }
